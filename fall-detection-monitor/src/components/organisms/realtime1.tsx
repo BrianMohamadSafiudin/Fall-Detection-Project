44704613ae747f75276f9436e2735970e28498b7
BrianMohamadSafiudin <brianms2004@gmail.com>
2024-06-13 11:24:46 +0700

    feat: Update Diagram Path

--- fall-detection-monitor/src/components/organisms/realtime1.tsx
+++ fall-detection-monitor/src/components/organisms/realtime1.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { database } from "../../firebaseConfig";
+import { database } from "../../firebaseconfig.js";
 import { ref, onValue } from "firebase/database";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
